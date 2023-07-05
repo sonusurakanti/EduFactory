0970b96bdf1cee2dac4998aec8c8ccc9e0025042
Surakanti Sonu Reddy <92659839+sonusurakanti@users.noreply.github.com>
2023-07-05 23:43:57 +0530

    Add files via upload

---
+++ payment/pay.js
@@ -0,0 +1,11 @@
+$(function () {
+  $('[data-toggle="popover"]').popover();
+
+  $("#cvc").on("click", function () {
+    if ($(".cvc-preview-container").hasClass("hide")) {
+      $(".cvc-preview-container").removeClass("hide");
+    } else {
+      $(".cvc-preview-container").addClass("hide");
+    }
+  });
+});
